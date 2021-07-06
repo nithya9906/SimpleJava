8a776a682be49d4805db2b530a631aefefc7be64
nithya9906 <nithya699@gmail.com>
2021-07-06 20:36:28 +0530

    EmployeeMainClass

---
+++ EmployeeMain.java
@@ -0,0 +1,15 @@
+package com.revature.employeeProject;
+
+public class EmployeeMain {
+	public static void main(String[] args) {
+		Employee e1=new Employee(1,"Nithya");
+		Employee e2=new Employee(2,"Lakshmi");
+		Employee e3=new Employee(3,"Safwan");
+		System.out.println(e1);
+		System.out.println(e2);
+		System.out.println(e3);
+		
+		
+	}
+
+}
