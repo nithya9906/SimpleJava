17cd038e0a4b66e614117efdcb356301d2207ee4
nithya9906 <nithya699@gmail.com>
2021-07-06 20:34:03 +0530

    EmployeeClass

---
+++ Employee.java
@@ -0,0 +1,29 @@
+package com.revature.employeeProject;
+
+public class Employee {
+	private int empId;
+	private String name;
+	public int getEmpId() {
+		return empId;
+	}
+	public void setEmpId(int empId) {
+		this.empId = empId;
+	}
+	public String getName() {
+		return name;
+	}
+	public Employee(int empId, String name) {
+		super();
+		this.empId = empId;
+		this.name = name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	@Override
+	public String toString() {
+		return "Employee [empId=" + empId + ", name=" + name + "]";
+	}
+	
+
+}
